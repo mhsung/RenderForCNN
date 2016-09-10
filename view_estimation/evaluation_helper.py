d94d29b1c0f6fa47e666bda994feeffda157c8bf
Minhyuk Sung <smh0816@gmail.com>
2016-09-10 12:43:03 -0700

    Updated 'evaluation_helper.py'.

--- view_estimation/evaluation_helper.py
+++ view_estimation/evaluation_helper.py
@@ -23,83 +23,99 @@ from caffe_utils import *
 @output:
     write 3d viewpoint estimation results to output_result_file
 '''
-def viewpoint(img_filenames, class_idxs, output_result_file):
+
+
+# def viewpoint(img_filenames, class_idxs, output_result_file):
+def viewpoint(img_filenames, class_idx, output_result_files):
     batch_size = g_test_batch_size
     model_params_file = g_caffe_param_file
     model_deploy_file = g_caffe_deploy_file
     result_keys = g_caffe_prob_keys
     resize_dim = g_images_resize_dim
     image_mean_file = g_image_mean_file
-    
+
     # ** NETWORK FORWARD PASS **
-    probs_lists = batch_predict(model_deploy_file, model_params_file, batch_size, result_keys, img_filenames, image_mean_file, resize_dim)
-    
+    probs_lists = batch_predict(model_deploy_file, model_params_file,
+                                batch_size, result_keys, img_filenames,
+                                image_mean_file, resize_dim)
+
     # EXTRACT PRED FROM PROBS
     preds = []
     for k in range(len(result_keys)):
         preds.append([])
     for i in range(len(img_filenames)):
-        class_idx = class_idxs[i]
+        # class_idx = class_idxs[i]
+
         # pred is the class with highest prob within
         # class_idx*360~class_idx*360+360-1
         for k in range(len(result_keys)):
             probs = probs_lists[k][i]
-            probs = probs[class_idx*360:(class_idx+1)*360]
-            pred = probs.argmax() + class_idx*360
+            probs = probs[class_idx * 360:(class_idx + 1) * 360]
+            pred = probs.argmax() + class_idx * 360
             preds[k].append(pred)
-    
+
     # OUTPUT: apred epred tpred
-    fout = open(output_result_file, 'w')
+    # fout = open(output_result_file, 'w')
+    # for i in range(len(img_filenames)):
+    #    fout.write('%d %d %d\n' % (preds[0][i] % 360, preds[1][i] % 360, preds[2][i] % 360))
+    # fout.close()
+
     for i in range(len(img_filenames)):
-        fout.write('%d %d %d\n' % (preds[0][i] % 360, preds[1][i] % 360, preds[2][i] % 360))
-    fout.close()
-    
-    
-    # OUTPUT: log file
-    log_output_filename = output_result_file+'.log'
-    fout = open(log_output_filename, 'w')
-    fout.write("output_result_file: %s\nresize_dim: %d\n" % (output_result_file, resize_dim))
-    fout.write('model_deploy_file_3dview: %s\n' % (model_deploy_file))
-    fout.write('model_params_file_3dview: %s\n' % (model_params_file))
-    fout.close()
+        print 'Saving "' + output_result_files[i] + '"'
+        fout = open(output_result_files[i], 'w')
+        fout.write('%d %d %d\n' % (
+        preds[0][i] % 360, preds[1][i] % 360, preds[2][i] % 360))
+        fout.close()
+
+
+        # OUTPUT: log file
+        # log_output_filename = output_result_file+'.log'
+        # fout = open(log_output_filename, 'w')
+        # fout.write("output_result_file: %s\nresize_dim: %d\n" % (output_result_file, resize_dim))
+        # fout.write('model_deploy_file_3dview: %s\n' % (model_deploy_file))
+        # fout.write('model_params_file_3dview: %s\n' % (model_params_file))
+        # fout.close()
 
 
 '''
 @brief:
     get local maximums from an 1-D array of angles (e.g. if len(prob)=360 then 0==360)
-@input: 
+@input:
     prob: N*1 array
     num_bin: local area width
     diff_threshold: regard close preds as duplicates
-@output: 
-    preds_list: a list of (pred,prob-for-the-pred) tuples 
+@output:
+    preds_list: a list of (pred,prob-for-the-pred) tuples
 '''
+
+
 def get_top_preds(prob, num_bin=8, diff_threshold=10):
     N = len(prob)
-    bin_width = N/num_bin
-    preds_list = [] # list of tuples of (<pred>, <prob-of-the-pred>)
+    bin_width = N / num_bin
+    preds_list = []  # list of tuples of (<pred>, <prob-of-the-pred>)
     preds_list.append((prob.argmax(), max(prob)))
 
-    for i in range(0,num_bin):
+    for i in range(0, num_bin):
         # get local top pred
-        prob_bin = prob[i*bin_width : (i+1)*bin_width]
-        local_pred = prob_bin.argmax() + i*bin_width
+        prob_bin = prob[i * bin_width: (i + 1) * bin_width]
+        local_pred = prob_bin.argmax() + i * bin_width
         local_prob = max(prob_bin)
 
         # verify
         duplicate = 0
         for i, ituple in enumerate(preds_list):
             ipred, iprob = ituple
-            if min(abs(local_pred - ipred), N - abs(local_pred - ipred)) < diff_threshold:
+            if min(abs(local_pred - ipred),
+                   N - abs(local_pred - ipred)) < diff_threshold:
                 if local_prob > iprob:
-                    preds_list[i] = (local_pred, local_prob) # exchange to bigger one
-                duplicate = 1 # too close to existed pred
+                    preds_list[i] = (
+                    local_pred, local_prob)  # exchange to bigger one
+                duplicate = 1  # too close to existed pred
                 break
         if duplicate == 0:
             preds_list.append((local_pred, local_prob))
-    preds_list = sorted(preds_list, key=lambda item:item[1], reverse=True)
+    preds_list = sorted(preds_list, key=lambda item: item[1], reverse=True)
     return preds_list
-
 
 
 '''
@@ -112,8 +128,10 @@ def get_top_preds(prob, num_bin=8, diff_threshold=10):
     return topk_viewpoints - a length-topk list of tuples of (<viewpoint-tuple>, <confidence>), where
     <viewpoint-tuple> is a length-3 tuples of azimuth, elevation and tilt angles in degree
 '''
+
+
 def get_topk_viewpoints(probs_3dview, topk):
-    assert(topk>=1)
+    assert (topk >= 1)
     viewpoints_list = []
 
     # secure softmax. used to normalize an array to 0~1 with sum as 1
@@ -123,32 +141,32 @@ def get_topk_viewpoints(probs_3dview, topk):
         return out
 
     # Rank by prob-azimuth * prob-elevation * prob-tilt
-    #for n in range(10):
+    # for n in range(10):
     #    if n*n*n >= topk: break
-    #probs_3dview = [my_softmax(prob) for prob in probs_3dview]
-    #preds_list_azimuth = get_top_preds(probs_3dview[0], 16, 45)[0:n]
-    #preds_list_elevation = get_top_preds(probs_3dview[1], 24, 10)[0:n]
-    #preds_list_tilt = get_top_preds(probs_3dview[2], 36, 5)[0:n]
-    #for i in range(n):
+    # probs_3dview = [my_softmax(prob) for prob in probs_3dview]
+    # preds_list_azimuth = get_top_preds(probs_3dview[0], 16, 45)[0:n]
+    # preds_list_elevation = get_top_preds(probs_3dview[1], 24, 10)[0:n]
+    # preds_list_tilt = get_top_preds(probs_3dview[2], 36, 5)[0:n]
+    # for i in range(n):
     #    for j in range(n):
     #        for k in range(n):
     #            apred,aprob = preds_list_azimuth[i]
     #            epred,eprob = preds_list_elevation[j]
     #            tpred,tprob = preds_list_tilt[k]
     #            viewpoints_list.append(((apred,epred,tpred), np.log(aprob)+np.log(eprob)+np.log(tprob)))
-    
+
     # Rank by azimuth and a bit of elevation
     preds_list_azimuth = get_top_preds(probs_3dview[0], 16, 45)
-    for i in range(min(topk,len(preds_list_azimuth))):
-        apred,aprob = preds_list_azimuth[i]
-        viewpoints_list.append(((apred, probs_3dview[1].argmax(), probs_3dview[2].argmax()), aprob))
-        
+    for i in range(min(topk, len(preds_list_azimuth))):
+        apred, aprob = preds_list_azimuth[i]
+        viewpoints_list.append(((apred, probs_3dview[1].argmax(),
+                                 probs_3dview[2].argmax()), aprob))
 
-    viewpoints_list = sorted(viewpoints_list, key=lambda item:item[1], reverse=True)
+    viewpoints_list = sorted(viewpoints_list, key=lambda item: item[1],
+                             reverse=True)
     topk_viewpoints = viewpoints_list[0:topk]
 
     return topk_viewpoints
-    
 
 
 '''
@@ -160,20 +178,22 @@ def get_topk_viewpoints(probs_3dview, topk):
     topk (K) - integer number (10=>K>=1). choose top K prediction results (in default, set to 1)
     output_result_file - string. output estimated viewpoints into this file
 @output:
-    return preds - a lenght-len(img_filenames) list of length-topk list of tuples 
+    return preds - a lenght-len(img_filenames) list of length-topk list of tuples
         of (<viewpoint-tuple>, <confidence>), ranked by confidence (high to low),
         where <viewpoint-tuple> is a length-3 tuple of azimuth, elevation, tilt angles in degree
 
         e.g. for len(img_filenames)=3 and topk=2, preds is like
-            [ [((a,e,t),c),((a,e,t),c)], 
-              [((a,e,t),c),((a,e,t),c)], 
+            [ [((a,e,t),c),((a,e,t),c)],
+              [((a,e,t),c),((a,e,t),c)],
               [((a,e,t),c),((a,e,t),c)] ]
 
     if output_result_file is not None
-        write 3d viewpoint estimation results to output_result_file, each line has Kx(3+1) numbers as 
+        write 3d viewpoint estimation results to output_result_file, each line has Kx(3+1) numbers as
         "<azimuth-top1> <elevation-top1> <tilt-top1> <confidence-top1> <azimuth-top2> <elevation-top2> <tilt-top2> <confidence-top2> ..."
         write output log file to <output_result_file>.log
 '''
+
+
 def viewpoint_topk(img_filenames, class_idxs, topk=1, output_result_file=None):
     batch_size = g_test_batch_size
     model_params_file = g_caffe_param_file
@@ -181,12 +201,14 @@ def viewpoint_topk(img_filenames, class_idxs, topk=1, output_result_file=None):
     result_keys = g_caffe_prob_keys
     resize_dim = g_images_resize_dim
     image_mean_file = g_image_mean_file
-    assert(topk>=1 and topk<=10) # from 1^3 to 10^3
-    assert(len(result_keys) == 3) #azimuth,elevation,tilt
-    
+    assert (topk >= 1 and topk <= 10)  # from 1^3 to 10^3
+    assert (len(result_keys) == 3)  # azimuth,elevation,tilt
+
     # ** NETWORK FORWARD PASS **
-    probs_lists = batch_predict(model_deploy_file, model_params_file, batch_size, result_keys, img_filenames, image_mean_file, resize_dim)
-    
+    probs_lists = batch_predict(model_deploy_file, model_params_file,
+                                batch_size, result_keys, img_filenames,
+                                image_mean_file, resize_dim)
+
     # EXTRACT PRED FROM PROBS
     preds = []
     for k in range(len(img_filenames)):
@@ -195,41 +217,87 @@ def viewpoint_topk(img_filenames, class_idxs, topk=1, output_result_file=None):
         class_idx = class_idxs[i]
 
         # probs_3dview is length-3 list of azimuth, elevation and tilt probs (each is length-360 list)
-        probs_3dview = [] 
+        probs_3dview = []
         for k in range(len(result_keys)):
             # probs for class_idx:
             # class_idx*360~class_idx*360+360-1
             probs = probs_lists[k][i]
-            probs = probs[class_idx*360:(class_idx+1)*360]
+            probs = probs[class_idx * 360:(class_idx + 1) * 360]
             probs_3dview.append(probs)
-       
+
         # get topk viewpoints: length-topk list of lenght-3 tuples
         topk_viewpoints = get_topk_viewpoints(probs_3dview, topk)
         preds[i] = topk_viewpoints
-    
+
     if output_result_file is not None:
         # OUTPUT: apred epred tpred
         fout = open(output_result_file, 'w')
         for i in range(len(img_filenames)):
-            topk_views = preds[i] 
+            topk_views = preds[i]
             for k in range(len(topk_views)):
-                va,ve,vt = topk_views[k][0]
+                va, ve, vt = topk_views[k][0]
                 confidence = topk_views[k][1]
-                fout.write('%d %d %d %f ' % (va,ve,vt,confidence))
+                fout.write('%d %d %d %f ' % (va, ve, vt, confidence))
             fout.write('\n')
         fout.close()
-        
-        
+
         # OUTPUT: log file
-        log_output_filename = output_result_file+'.log'
+        log_output_filename = output_result_file + '.log'
         fout = open(log_output_filename, 'w')
-        fout.write("output_result_file: %s\nresize_dim: %d\ntopk: %d\n" % 
-                (output_result_file, resize_dim, topk))
+        fout.write("output_result_file: %s\nresize_dim: %d\ntopk: %d\n" %
+                   (output_result_file, resize_dim, topk))
         fout.write('model_deploy_file_3dview: %s\n' % (model_deploy_file))
         fout.write('model_params_file_3dview: %s\n' % (model_params_file))
         fout.close()
 
     return preds
+
+
+# @mhsung
+def viewpoint_pred(img_filenames, class_idx, output_result_files):
+    batch_size = g_test_batch_size
+    model_params_file = g_caffe_param_file
+    model_deploy_file = g_caffe_deploy_file
+    result_keys = g_caffe_prob_keys
+    resize_dim = g_images_resize_dim
+    image_mean_file = g_image_mean_file
+    assert (len(result_keys) == 3)  # azimuth,elevation,tilt
+
+    # ** NETWORK FORWARD PASS **
+    probs_lists = batch_predict(model_deploy_file, model_params_file,
+                                batch_size, result_keys, img_filenames,
+                                image_mean_file, resize_dim)
+
+    # EXTRACT PRED FROM PROBS
+    probs_3dview_all = []
+    for k in range(len(img_filenames)):
+        probs_3dview_all.append([])
+    for i in range(len(img_filenames)):
+        # class_idx = class_idxs[i]
+
+        # probs_3dview is length-3 list of azimuth, elevation and tilt probs (each is length-360 list)
+        probs_3dview = []
+        for k in range(len(result_keys)):
+            # probs for class_idx:
+            # class_idx*360~class_idx*360+360-1
+            probs = probs_lists[k][i]
+            probs = probs[class_idx * 360:(class_idx + 1) * 360]
+            probs_3dview.append(probs)
+        probs_3dview_all[i] = probs_3dview
+
+    for i in range(len(img_filenames)):
+        print 'Saving "' + output_result_files[i] + '"'
+        np.save(output_result_files[i], probs_3dview_all[i])
+
+    # OUTPUT: log file
+    # log_output_filename = output_result_file + '.log'
+    # fout = open(log_output_filename, 'w')
+    # fout.write("output_result_file: %s\nresize_dim: %d\ntopk: %d\n" %
+    #            (output_result_file, resize_dim, topk))
+    # fout.write('model_deploy_file_3dview: %s\n' % (model_deploy_file))
+    # fout.write('model_params_file_3dview: %s\n' % (model_params_file))
+    # fout.close()
+
 
 '''
 @brief:
@@ -242,38 +310,47 @@ def viewpoint_topk(img_filenames, class_idxs, topk=1, output_result_file=None):
     result_folder - result .mat files will be saved there
 @output:
     output .mat results (see combine_bbox_view.m for more details) into result_folder.
-    display AVP-NV results 
+    display AVP-NV results
 @dependency:
     combine_bbox_view.m (output <clsname>_v<NV>.mat like chair_v8.mat)
     test_det.m  (assumes .mat files available in folder)
 '''
-def test_avp_nv(cls_names, img_name_file_list, det_bbox_mat_file_list, result_folder):
+
+
+def test_avp_nv(cls_names, img_name_file_list, det_bbox_mat_file_list,
+                result_folder):
     C = len(cls_names)
-    assert(C == len(img_name_file_list))
-    assert(C == len(det_bbox_mat_file_list))
+    assert (C == len(img_name_file_list))
+    assert (C == len(det_bbox_mat_file_list))
     class_idxs = [g_shape_names.index(name) for name in cls_names]
 
     if not os.path.exists(result_folder):
         os.mkdir(result_folder)
-    
-    for i,class_idx in enumerate(class_idxs):
-        img_filenames = [x.rstrip().split(' ')[0] for x in open(img_name_file_list[i])]
+
+    for i, class_idx in enumerate(class_idxs):
+        img_filenames = [x.rstrip().split(' ')[0] for x in
+                         open(img_name_file_list[i])]
         class_idxs = [int(class_idx) for _ in range(len(img_filenames))]
-        
+
         # viewpoint estimation with caffe python
         # wirte <result_folder>/<class_name>_pred_view.txt
-        output_result_file = os.path.join(result_folder, cls_names[i]+'_pred_view.txt')
+        output_result_file = os.path.join(result_folder,
+                                          cls_names[i] + '_pred_view.txt')
         viewpoint(img_filenames, class_idxs, output_result_file)
-   
+
         # combine det and view
-        matlab_cmd = "addpath('%s'); combine_bbox_view('%s','%s','%s','%s', %d);" % (BASE_DIR, cls_names[i], det_bbox_mat_file_list[i], output_result_file, result_folder, 0)
+        matlab_cmd = "addpath('%s'); combine_bbox_view('%s','%s','%s','%s', %d);" % (
+        BASE_DIR, cls_names[i], det_bbox_mat_file_list[i], output_result_file,
+        result_folder, 0)
         print matlab_cmd
-        os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (g_matlab_executable_path, matlab_cmd))
-    
+        os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (
+        g_matlab_executable_path, matlab_cmd))
+
     # compute AVP-NV for all classes
     matlab_cmd = "addpath('%s'); test_det('%s');" % (BASE_DIR, result_folder)
     print matlab_cmd
-    os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (g_matlab_executable_path, matlab_cmd))
+    os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (
+    g_matlab_executable_path, matlab_cmd))
 
 
 '''
@@ -291,24 +368,30 @@ def test_avp_nv(cls_names, img_name_file_list, det_bbox_mat_file_list, result_fo
 @dependency:
     test_gt.m (assumes <clsname>_pred_view.txt available in folder)
 '''
-def test_vp_acc(cls_names, img_name_file_list, result_folder, view_label_folder):
-    assert(len(cls_names) == len(img_name_file_list))
+
+
+def test_vp_acc(cls_names, img_name_file_list, result_folder,
+                view_label_folder):
+    assert (len(cls_names) == len(img_name_file_list))
     class_idxs = [g_shape_names.index(name) for name in cls_names]
 
     if not os.path.exists(result_folder):
         os.mkdir(result_folder)
-    
-    for i,class_idx in enumerate(class_idxs):
-        tmp_img_filenames = [x.rstrip().split(' ')[0] for x in open(img_name_file_list[i])]
+
+    for i, class_idx in enumerate(class_idxs):
+        tmp_img_filenames = [x.rstrip().split(' ')[0] for x in
+                             open(img_name_file_list[i])]
         tmp_class_idxs = [int(class_idx) for _ in range(len(tmp_img_filenames))]
-        
+
         # viewpoint estimation with caffe python
         # wirte <result_folder>/<class_name>_pred_view.txt
-        output_result_file = os.path.join(result_folder, cls_names[i]+'_pred_view.txt')
+        output_result_file = os.path.join(result_folder,
+                                          cls_names[i] + '_pred_view.txt')
         viewpoint(tmp_img_filenames, tmp_class_idxs, output_result_file)
-   
 
     # compute Acc and MedErr
-    matlab_cmd = "addpath('%s'); test_gt('%s','%s');" % (BASE_DIR, result_folder, view_label_folder)
+    matlab_cmd = "addpath('%s'); test_gt('%s','%s');" % (
+    BASE_DIR, result_folder, view_label_folder)
     print matlab_cmd
-    os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (g_matlab_executable_path, matlab_cmd))
+    os.system('%s -nodisplay -r "try %s ; catch; end; quit;"' % (
+    g_matlab_executable_path, matlab_cmd))
